fff6eee1aad54ce73ec98e884192780fc81765b3
Schahriar SaffarShargh <info@schahriar.com>
2017-02-10 22:38:30 -0800

    Added missing test cases for Pipeline Class

--- test/Pipeline.unit.js
+++ test/Pipeline.unit.js
@@ -2,7 +2,6 @@ const EventEmitter = require('events').EventEmitter;
 let PassThrough = require('stream').PassThrough;
 let pipeline, interface;
 let lastMessage = "";
-let stream = new PassThrough();
 
 describe("Pipeline Unit Tests", function () {
   it('should create a new Pipeline', function () {
@@ -14,6 +13,8 @@ describe("Pipeline Unit Tests", function () {
   });
 
   it('should pipe to a processor', function (done) {
+    const stream = new PassThrough();
+
     pipeline.pipe((chunk, line, next) => {
       lastMessage = chunk;
       next(chunk);
@@ -24,10 +25,61 @@ describe("Pipeline Unit Tests", function () {
     stream.write('testChunk');
     setImmediate(() => {
       expect(lastMessage.toString('utf8')).to.be.equal('testChunk');
+      // End stream
+      stream.end();
       done();
     });
   });
 
-  it.skip('should catch errors');
-  it.skip('should trigger next event');
+  it('should trigger next event', function (done) {
+    const stream = new PassThrough();
+
+    pipeline.once('next', () => {
+      // End stream
+      stream.end();
+      done();
+    });
+    
+    intf.consume(stream);
+
+    stream.write('test');
+  });
+
+  it('should catch sync errors', function (done) {
+    const stream = new PassThrough();
+
+    pipeline.pipe((chunk, line, next) => {
+      if (chunk && (chunk.toString('utf8') === 'throwSyncError')) throw new Error('throwSyncError');
+      next(chunk);
+    }).once('error', (error) => {
+      expect(error).to.be.an.instanceof(Error);
+      expect(error).to.have.property('message', 'throwSyncError');
+      // End stream
+      stream.end();
+      done();
+    });
+
+    intf.consume(stream);
+
+    stream.write('throwSyncError');
+  });
+
+  it('should catch async errors', function (done) {
+    const stream = new PassThrough();
+
+    pipeline.pipe((chunk, line, next) => {
+      if (chunk && (chunk.toString('utf8') === 'throwAsyncError')) return line.throw(new Error('throwAsyncError'));
+      next(chunk);
+    }).once('error', (error) => {
+      expect(error).to.be.an.instanceof(Error);
+      expect(error).to.have.property('message', 'throwAsyncError');
+      // End stream
+      stream.end();
+      done();
+    });
+
+    intf.consume(stream);
+
+    stream.write('throwAsyncError');
+  });
 });
