2287eee62d8b5da8943d10ec0d10f7ef181a5c32
Schahriar SaffarShargh <info@schahriar.com>
2017-02-11 22:04:53 -0800

    Fixed bad markdown formatting

--- docs/versionUpdate.js
+++ docs/versionUpdate.js
@@ -6,7 +6,7 @@ const README = fs.readFileSync('./README.md').toString('utf8');
 const vStart = README.indexOf('<!--VERSION_START-->') + '<!--VERSION_START-->'.length;
 const vEnd = README.indexOf('<!--VERSION_END-->');
 const versionMors = morjs.encode(package.version, {mode: 'simple'});
-const versionString = `v${package.version} \`${versionMors}\``;
+const versionString = `\nv${package.version} \`${versionMors}\`\n`;
 const currentString = README.slice(vStart, vEnd);
 
 fs.writeFileSync('./README.md', README.slice(0, vStart) + versionString + README.slice(vStart + versionString.length));
